ab986e5eae779538e025ab859b919213b53ca87a
Michael Osostowicz <webensteinmx@gmail.com>
2016-03-07 16:19:19 +1100

    Fix bugs with regards to referencing jquery object without  wrapper

--- JS/TextFlash.js
+++ JS/TextFlash.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function(){
-$(".flash-text").each(function( index, element ) {
-		if (element.children().length == 0) {
-			setInterval(function() {
-			element.toggle();
-		}, 1000);
-	}
-});
+	$(".flash-text").each(function( index, element ) {
+			if ($(element).children().length == 0) {
+				setInterval(function() {
+				$(element).toggle();
+			}, 1000);
+		}
+	});
 });
